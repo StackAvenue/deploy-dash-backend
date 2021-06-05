3cb3bffc613946e4cefc25e8c945edadb951ad60
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-05 13:11:23 +0530

    migrations for user model

---
+++ db/migrate/20210604120323_create_users.rb
@@ -0,0 +1,23 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :access_token
+      t.string :login
+      t.integer :git_id
+      t.text :avatar_url
+      t.text :url
+      t.text :organisations_url
+      t.text :received_events_url
+      t.string :name
+      t.string :company
+      t.text :blog
+      t.string :location
+      t.string :email
+      t.text :bio
+      t.datetime :github_account_created_at
+      t.string :twitter_username
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_04_120323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "access_token"
+    t.string "login"
+    t.integer "git_id"
+    t.text "avatar_url"
+    t.text "url"
+    t.text "organisations_url"
+    t.text "received_events_url"
+    t.string "name"
+    t.string "company"
+    t.text "blog"
+    t.string "location"
+    t.string "email"
+    t.text "bio"
+    t.datetime "github_account_created_at"
+    t.string "twitter_username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end

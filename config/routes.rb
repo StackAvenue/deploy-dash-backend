7b0970fe3936f982919b74fac6d0acd0341674e6
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-05 13:10:30 +0530

    added routes for user and gitoauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :github_oauth do
+        get :authorise_user, on: :collection
+      end
+      resources :users, only: [:show]
+    end
+  end
+
+  get '/api/v1/users', to: 'api/v1/users#show'
 end

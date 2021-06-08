a32b2e931632ae4b43eccb612e42e421d3f3e86a
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-08 18:18:01 +0530

    response controller to handle reponses

---
+++ app/controllers/api/v1/response_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::ResponseController < ApplicationController
+
+  def render_success(data: nil, status: 200)
+    render json: data, status: status
+  end
+  
+  def render_error(message: nil, status: 422)
+    render json: { message: message}  , status: status
+  end
+  
+  def render_unauthorized(message: nil, status: 401)
+    render json: { message: message}  , status: status
+  end
+ 
+end

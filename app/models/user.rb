a9c3a8db9a1fa45b5b418fd7073de4abc61de586
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-08 18:15:41 +0530

    updated users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,14 @@ class User < ApplicationRecord
   class << self
     def register_oauth_user(oauth_service_name, oauth_data)
       user_details = OAUTH_SERVICE_NAME[oauth_service_name].constantize.get_basic_user_details(oauth_data)
-      return user = User.create_user(user_details) unless User.exists(user_details)
-      return user = User.update(access_token: user_details[:access_token])
+      return user_details unless user_details[:success]
+      return user = User.update_details(access_token: user_details[:access_token]) if User.exists(user_details)[:success]
+      return user = User.create_user(user_details)
     end
 
     def get_user_details(access_token)
       user_details = User.where(access_token: "#{access_token}")
+      return render_error(message: 'Invalid or empty access token!!') if access_token.nil?
       user_details
     end
       
@@ -42,11 +44,24 @@ class User < ApplicationRecord
       bio: user_details[:user_details]["bio"], 
       github_account_created_at: user_details[:user_details]["created_at"], 
       twitter_username: user_details[:user_details]["twitter_username"])
+      user = {user: user}
+      {data: user, success: true}
     end
 
     def exists(user_details)
       user =  User.where(git_id: user_details[:user_details]["id"], login: user_details[:user_details]["login"] )
-      return true if user.present?
+      return {success: true} if user.present?
+      return {success: false}
+    end
+
+    def update_details(access_token)
+      user = User.update(access_token: access_token[:access_token])
+      user = {user: User.to_json(user)}
+      {data: user, success: true}
+    end
+
+    def to_json(user)
+      user = user.as_json.first
     end
   end
  

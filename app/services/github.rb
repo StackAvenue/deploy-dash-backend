b18249b381a70bd6901bade4006bb52b7f5ed929
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-08 18:17:22 +0530

    added proper return for proper response handeling

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,28 +1,26 @@
 class Github
   def self.get_basic_user_details(oauth_data)
     code = oauth_data[:code]
-    # return Response.error('No code available!') if oauth_data[:code].blank?
-    access_token = get_access_token(code)
-    # return access_token unless access_token[:success]
-    # access_token = access_token[:data][:access_token
-    user_details = get_user_details(access_token)
+    return {success: false , message: 'No code available!'} if oauth_data[:code].blank?
+    access_data = get_access_token(code)
+    return {success: access_data[:success], message: access_data[:error]} unless access_data[:success]
+    user_details = get_user_details(access_data)
     user_details = user_details.slice("login","id","avatar_url", "url", "organisations_url", "received_events_url", "name", "company", "blog", "location", "email", "bio", "created_at", "twitter_username")
-    return {user_details: user_details, access_token: "#{access_token}"}
-  # rescue StandardError => e
-  #   return Response.error(500, 'NetworkReach', e.message)
+    return {success: true, user_details: user_details, access_token: "#{access_data[:access_token]}"}
   end
 
   def self.get_access_token(code)
     url = "https://github.com/login/oauth/access_token"
     response = RestClient::Request.execute(method: :post, url: url, headers: { params: {client_id: "#{ENV['CLIENT_ID']}", client_secret: "#{ENV['CLIENT_SECRET']}", code: "#{code}"}, Accept: 'application/json' })
-    access_token = JSON.parse(response.body)
-    access_token = access_token["access_token"]
-    return access_token
+    json_response = JSON.parse(response.body)
+    access_token = json_response["access_token"]
+    return {success: true, access_token: access_token} unless access_token.nil?
+    return {success: false, error: json_response["error_description"]}
   end
 
-  def self.get_user_details(access_token)
+  def self.get_user_details(access_data)
     url = 'https://api.github.com/user'
-    response = RestClient::Request.execute(method: :get, url: url, headers: { Authorization: "token #{access_token}"})
+    response = RestClient::Request.execute(method: :get, url: url, headers: { Authorization: "token #{access_data[:access_token]}"})
     user_details = JSON.parse(response.body)
     return user_details
   end

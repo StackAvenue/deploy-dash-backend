77e92c6dc68ca1db5149ab4abe2cb9d693f07531
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-08 18:13:56 +0530

    upadated authorised user method

--- app/controllers/api/v1/github_oauth_controller.rb
+++ app/controllers/api/v1/github_oauth_controller.rb
@@ -1,9 +1,10 @@
-class Api::V1::GithubOauthController < ApplicationController
+class Api::V1::GithubOauthController < Api::V1::ResponseController
 
   def authorise_user
     code = params["code"]
     user = User.register_oauth_user(User::OAUTH_TYPES[:github], { code: code })
-    render json: user
+    render_unauthorized(message: user[:message]) && return unless user[:success]
+    render_success(data: user[:data]) 
   end
   
 end

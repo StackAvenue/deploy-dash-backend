4171676a768283e120ba53687e6b71100daff33a
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-08 19:25:14 +0530

    updated method to get proper details of user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::UsersController < Api::V1::ResponseController
   def show
     access_token = params["access_token"]
     user_details = User.get_user_details(access_token)
-    render json: user_details
+    render_error(message: user_details[:message]) && return unless user_details[:success]
+    render_success(data: user_details[:data])
   end
 
 end

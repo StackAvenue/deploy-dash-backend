35f550c63533c81239d88e065cc1d86bc2ccaaf7
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-09 16:01:39 +0530

    removed extra space

--- app/controllers/api/v1/github_oauth_controller.rb
+++ app/controllers/api/v1/github_oauth_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::GithubOauthController < Api::V1::ResponseController
     code = params['code']
     user = User.register_oauth_user(User::OAUTH_TYPES[:github], { code: code })
     render_unauthorized(message: user[:message]) && return unless user[:success]
-    
+
     render_success(data: user[:data])
   end
 end

f1d57a21f2a4fcac7899f23179df365180dcd1ce
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-11 22:13:22 +0530

    branches action to get branches and get access token from headers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,25 @@
 class Api::V1::UsersController < Api::V1::ResponseController
   def show
-    access_token = params['access_token']
-    user_details = User.get_user_details(access_token)
-    render_error(message: user_details[:message]) && return unless user_details[:success]
+    user_details = User.get_user_details(@access_token)
+    render_unauthorized(message: user_details[:message]) && return unless user_details[:success]
 
     render_success(data: user_details[:data])
   end
 
   def repositories
-    access_token = params['access_token']
-    repositories = User.get_repositories(User::OAUTH_TYPES[:github], access_token)
-    render_error(message: repositories[:message]) && return unless repositories[:success]
+    repositories = User.get_repositories(User::OAUTH_TYPES[:github], @access_token)
+    render_unauthorized(message: repositories[:message]) && return unless repositories[:success]
 
     render_success(data: repositories[:data])
+  end
+
+  def branches
+    branch_name = params['full_name']
+    branches = User.get_branches(User::OAUTH_TYPES[:github], @access_token, branch_name)
+    return render_unauthorized(message: branches[:message]) if branches[:message] == 'Invalid access token!!'
+
+    return render_error(message: branches[:message]) if branches[:message] == 'Invalid or empty branch name'
+
+    render_success(data: branches[:data])
   end
 end

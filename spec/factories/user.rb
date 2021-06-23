7252ff2773def2c7e363fcb80fa76bcd4166b777
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-23 11:17:34 +0530

    user data for test

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: false
+
 FactoryBot.define do
   factory :user do
     access_token { Faker::Number.number }
-    git_id { Faker::Number.number }
+    git_id { Faker::Number.number(digits: 4) }
     login { Faker::Name.name }
     avatar_url { Faker::Name.name }
     url { Faker::Name.name }
     repos_url { Faker::Name.name }
     organisations_url { Faker::Name.name }
-    repositories { Faker::Name.name }
+    repositories { [Faker::Name.name] }
     received_events_url { Faker::Name.name }
     company { Faker::Name.name }
     name { Faker::Name.name }
@@ -15,7 +17,7 @@ FactoryBot.define do
     location { Faker::Name.name }
     email { Faker::Name.name }
     bio { Faker::Name.name }
-    github_account_created_at
+    github_account_created_at { Faker::Name.name }
     twitter_username { Faker::Name.name }
-  end 
-end 
+  end
+end

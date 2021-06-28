3610312df1a9da7cbed84a04e1e532ea44729aac
apeksha2697 <apekshasingh664@gmail.com>
2021-06-28 20:22:26 +0530

    removed code variable

--- app/controllers/api/v1/github_oauth_controller.rb
+++ app/controllers/api/v1/github_oauth_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::GithubOauthController < Api::V1::ResponseController
   def authorise_user
-    code = params['code']
-    user = User.register_oauth_user(User::OAUTH_TYPES[:github], { code: code })
+    user = User.register_oauth_user(User::OAUTH_TYPES[:github], { code: params['code'] })
     render_unauthorized(message: user[:message]) && return unless user[:success]
 
     render_success(data: user[:data])

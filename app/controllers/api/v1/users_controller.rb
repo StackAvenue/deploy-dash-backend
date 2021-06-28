a172e2c0f5ee4d900e055d6bd4b65b871cb2760f
apeksha2697 <apekshasingh664@gmail.com>
2021-06-28 20:22:57 +0530

    removed access token variable

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < Api::V1::ResponseController
   def show
-    access_token = params['access_token']
-    user_details = User.get_user_details(access_token)
+    user_details = User.get_user_details(params['access_token'])
     render_error(message: user_details[:message]) && return unless user_details[:success]
 
     render_success(data: user_details[:data])

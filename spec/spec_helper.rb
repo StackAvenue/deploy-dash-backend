b1659a5f6b5e5e3481c43dae2c23c56f3107da87
apeksha2697 <apekshasingh664@gmail.com>
2021-07-12 10:30:51 +0530

    added webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -95,9 +95,4 @@ RSpec.configure do |config|
 =end
   require 'webmock/rspec'
   WebMock.disable_net_connect!(allow_localhost: true)
-  config.before(:each) do
-    stub_request(:get, /api.github.com/).
-      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-      to_return(status: 200, body: "stubbed response", headers: {})
-  end
 end
